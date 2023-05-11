9b44f860da9d9176cbf74378d34bdb55237add15
Annisa Agusti Andini <annisa.anndini@gmail.com>
2023-05-11 18:37:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a478529_pppb2_tugas8/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a478529_pppb2_tugas8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.a478529_pppb2_tugas8.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
